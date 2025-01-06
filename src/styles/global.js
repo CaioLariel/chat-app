ccfdf87f6a8781431ef7c33c8c4491470a254d16
caiolariel <cgfuentelima0277@gmail.com>
2025-01-06 00:07:17 -0300

    feat: create responsive

--- src/styles/global.js
+++ src/styles/global.js
@@ -11,6 +11,18 @@ const GlobalStyle = createGlobalStyle`
     height: 100vh;
     font-family:  'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   }
+
+  .chat-container {
+  display: flex;
+  flex-direction: row; /* Alinhar lado a lado em telas maiores */
+}
+
+@media (max-width: 768px) {
+  .chat-container {
+    flex-direction: column; /* Alinhar verticalmente em telas menores */
+  }
+}
+
 `;
 
 export default GlobalStyle;
